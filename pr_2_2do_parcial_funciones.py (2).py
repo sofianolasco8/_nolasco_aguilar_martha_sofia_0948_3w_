fe797b576c587786b7bf10389c5fd36a41b7bd52
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-10-09 22:45:22 -0700

    Add files via upload

---
+++ pr_2_2do_parcial_funciones.py (2).py
@@ -0,0 +1,20 @@
+print(" ")
+print("nolasco_aguilar_martha_sofia_0948_3w ")
+print(" ")
+
+def saludo():#se crea la funcion
+    
+    # indica  al usuario  ingresar su nombre
+    nombre = input("¿Cuál es tu nombre? ").strip()
+    
+    # Imprime el saludo personalizado
+    print(f"Hey  Hola {nombre}!")
+
+
+saludo()
+    
+    
+
+
+
+
